03053265ebd27d05e30d6a84ead3a84ae6a9c9e6
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-07-21 14:46:31 +0330

    feat : add CategoryMenuComponent

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import BackShadow from "@/components/BackShadow";
+import CategoryMenu from "@/components/CategoryMenu";
 import PrimaryButton from "@/components/PrimaryButton";
 import TakShopLogo from "@/components/TakShopLogo";
 import { useModal } from "@/context/LoginModalContext";
@@ -15,7 +16,7 @@ import { LiaLaptopSolid } from "react-icons/lia";
 import { MdDarkMode } from "react-icons/md";
 
 const Home = () => {
-  const [isActiveDropDown, setIsActiveDropDown] = useState(false);
+  const [isActiveCategoryMenu, setIsActiveCategoryMenu] = useState(false);
   
   const { isModalOpen, openModal, closeModal } = useModal();
 
@@ -42,189 +43,12 @@ const Home = () => {
             </li>
             <li
               className="header-menu_item gap-1 cursor-pointer group "
-              onMouseEnter={() => setIsActiveDropDown(true)}
-              onMouseLeave={() => setIsActiveDropDown(false)}
+              onMouseEnter={() => setIsActiveCategoryMenu(true)}
+              onMouseLeave={() => setIsActiveCategoryMenu(false)}
             >
               <span>دسته بندی کالاها</span>
               <IoIosArrowUp className="group-hover:-rotate-180 transition-transform" />
-              <section
-                className={`absolute translate-x-1/2 right-1/2 bg-white w-4/5 top-full transition-all ${isActiveDropDown ? "visible opacity-100 -translate-y-4" : " invisible translate-y-4 opacity-0"} rounded-b-md pl-4 flex h-min text-black cursor-default overflow-hidden`}
-              >
-                <section className="border-l-3 border-transparent [border-image:linear-gradient(to_bottom,#fff,#831fc1,#fff)_1] flex-1">
-                  <ul className="text-black flex flex-col h-full">
-                    <li className="dropdown-category_item">
-                      <CiMobile3 className="category-title_icon" />
-                      <span>موبایل</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <LiaLaptopSolid className="category-title_icon" />
-                      <span>لپ‌تاپ</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <IoGameControllerOutline className="category-title_icon" />
-                      <span>لوازم جانبی لپ‌تاپ</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <CiHeadphones className="category-title_icon" />
-                      <span>هدست و هدفون</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <AiOutlineCamera className="category-title_icon" />
-                      <span>دوربین</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <IoIosGitNetwork className="category-title_icon" />
-                      <span>اینترنت</span>
-                    </li>
-                    <li className="dropdown-category_item">
-                      <BsSmartwatch className="category-title_icon" />
-                      <span>ساعت هوشمند</span>
-                    </li>
-                  </ul>
-                </section>
-                <section className="flex-4 flex flex-col px-10  py-4 gap-6">
-                  <button className="cursor-pointer hover:text-button-hover transition-colors">
-                    <h2 className="flex items-center justify-center desktop-heading2">
-                      <CiHeart className="text-3xl" />
-                    </h2>
-                  </button>
-                  <Link
-                    href={"/"}
-                    className="flex items-center text-main-500 hover:text-main-700"
-                  >
-                    <h2 className="desktop-heading2">مشاهده همه محصولات </h2>
-                    <IoIosArrowUp className="-rotate-90 text-2xl" />
-                  </Link>
-                  <section className="flex justify-between">
-                    <section className="title-categories_container">
-                      <h3 className="title-category_heading">
-                        دسته‌بندی بر اساس برند
-                      </h3>
-                      <ul className="title-category_list">
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            آیفون
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            سامسونگ
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            شیائومی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            هواوی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            آنر
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            ریلمی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            نوکیا
-                          </Link>
-                        </li>
-                      </ul>
-                    </section>
-                    <section className="title-categories_container">
-                      <h3 className="title-category_heading">
-                        دسته‌بندی بر اساس قیمت
-                      </h3>
-                      <ul className="title-category_list">
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            زیر پنج میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بین پنج تا هفت میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بین هفت تا ده میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بین ده تا پونزده میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بین پونزده تا بیست میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بین بیست تا سی میلیون تومان
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            بالای سی میلیون تومان
-                          </Link>
-                        </li>
-                      </ul>
-                    </section>
-                    <section className="title-categories_container">
-                      <h3 className="title-category_heading">
-                        دسته‌بندی بر اساس عملکرد
-                      </h3>
-                      <ul className="title-category_list">
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            آیفون
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            سامسونگ
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            شیائومی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            هواوی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            آنر
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            ریلمی
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={"#"} className="category-list_item">
-                            نوکیا
-                          </Link>
-                        </li>
-                      </ul>
-                    </section>
-                  </section>
-                </section>
-              </section>
+             <CategoryMenu isActive={isActiveCategoryMenu}/>
             </li>
             <li className="header-menu_item">
               <Link href={"#"}>
@@ -307,7 +131,7 @@ const Home = () => {
           <PrimaryButton type="submit">ورود</PrimaryButton>
         </form>
       </section>
-      <BackShadow isActiveState={isActiveDropDown || isModalOpen} onClick={closeModal}/>
+      <BackShadow isActiveState={isActiveCategoryMenu || isModalOpen} onClick={closeModal}/>
     </>
   );
 };
